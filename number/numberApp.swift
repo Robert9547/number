eccd7bd8f2d6a24d7b623f751bdbb2376a367ed5
robert <00857002@email.ntou.edu.tw>
2022-04-19 17:30:11 +0800

    Initial Commit

---
+++ number/numberApp.swift
@@ -0,0 +1,17 @@
+//
+//  numberApp.swift
+//  number
+//
+//  Created by 賴冠宏 on 2022/4/19.
+//
+
+import SwiftUI
+
+@main
+struct numberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
